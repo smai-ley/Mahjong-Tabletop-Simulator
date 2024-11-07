cf0c5092aefa136c14ee1aaa96bfb69ce1138e2c
maipants <tkrkh43@gmail.com>
2024-11-06 21:10:30 -0500

    Add project files.

---
+++ Mahjong/Mahjong.cpp
@@ -0,0 +1,27 @@
+// Mahjong.cpp 
+
+#include <iostream>
+
+// void throwDice();
+
+// int assignOrder();
+
+int main()
+{
+    std::cout << "Welcome to Mahjong | Version 0.1 | Latest Commit: 11/6/2024 | Designed and Built by smai-ley\n";
+    std::cout << "Mahjong is played with 4 people! This version can only support one player so you will play against a computer.\n";
+    //std::cout << "The order of turns will be decided by a dice throw. The best throw will go first.\n";
+    std::cout << "You will start off first.\n";
+    
+    Player PLAYER = Player();
+    Computer COM2 = Computer();
+    Computer COM3 = Computer();
+    Computer COM4 = Computer();
+}
+
+//void throwDice() {
+    // std::cout << "MAHJONG: Throwing dice for all players.\n";
+    // random int between 2-12
+
+//}
+
---
+++ Mahjong/Player.h
@@ -0,0 +1,13 @@
+#pragma once
+class Player
+{
+private:
+	int turnOrder = 1;
+	bool mano = true;
+
+public:
+	// Linked list with 14 items
+
+
+};
+
